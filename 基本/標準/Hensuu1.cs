09bbca8eff746b7a2086b25a3ffb70841496949e
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-03-27 23:40:28 +0900

    Add files via upload

---
+++ 基本/標準/Hensuu1.cs
@@ -0,0 +1,20 @@
+﻿// 変数を使う
+using System;
+
+namespace Hensuu1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var player = "勇者";//var変数は、自動で型名を認識してくれる
+            Console.WriteLine(player);
+
+            Console.WriteLine(player + "荒野を歩いていた");
+            Console.WriteLine(player + "は、モンスターと戦った");
+
+            player = "戦士";
+            Console.WriteLine(player + "は、モンスターをたおした");
+        }
+    }
+}
---
+++ 基本/標準/random.cs
@@ -0,0 +1,21 @@
+﻿// 数の表示とサイコロ
+using System;
+
+namespace Randomnumber
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var random = new Random();
+            /*ランダムクラスをインスタンス化
+            インスタンスは、クラスを利用すること
+             newは、作るという意味*/
+
+            var number = random.Next(1, 101);
+            /*random.Next()は、0以上の数字を表示
+            Next(1,101)1から100まで(101未満)のランダムな数字*/
+            Console.WriteLine(number);
+        }
+    }
+}
