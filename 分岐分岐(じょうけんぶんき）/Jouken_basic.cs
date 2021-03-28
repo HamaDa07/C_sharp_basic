f2ba23aabe2eaa9c7c8fd22dfcb2267207932e14
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-03-28 23:21:27 +0900

    Add files via upload

---
+++ 分岐分岐(じょうけんぶんき）/Jouken_basic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if_basic2
+{
+    class jouken_basic2
+    {
+        public static void Main()
+        {
+            var time = 12;
+            if (time < 12)
+            {
+                Console.WriteLine("午前");    // 条件式が成立したときの処理
+            }
+            else if (time == 12)
+            {
+                Console.WriteLine("正午");
+            }
+            else
+            {
+                Console.WriteLine("午後");
+            }
+        }
+    }
+}
---
+++ 分岐分岐(じょうけんぶんき）/Omikuji.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Omikuji1
+{
+    class Omikuji
+    {
+        static void Main()
+        {
+            var random = new Random();
+            var omikuji = random.Next(1, 11); //  1 < =x <11
+
+            //Console.WriteLine(omikuji);
+            if (omikuji == 1)
+            {
+                Console.WriteLine("大吉");
+            }
+            else if (omikuji == 2)
+            {
+                Console.WriteLine("中吉");
+            }
+            else if (omikuji <= 4)
+            {
+                Console.WriteLine("小吉");//3,4
+            }
+            else if (omikuji <= 7)
+            {
+                Console.WriteLine("凶");//5.6.7
+            }
+            else
+            {
+                Console.WriteLine("大凶");
+            }
+        }
+    }
+}
