418dcd4a4a502fa25d18d73d45d790d71eee6633
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-03-29 23:31:22 +0900

    Add files via upload

---
+++ for_while_scan/For_basic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace For_basic
+{
+    class For_basic
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("hello world");
+            }
+            Console.WriteLine(i);
+
+        }
+    }
+}
---
+++ for_while_scan/For_plus_scan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For_plus_scan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            count = int.Parse(Console.ReadLine());
+            Console.WriteLine("データ個数" + count);
+
+            var data = "";
+            for (var i = 0; i < count; i++)
+            {
+                data = Console.ReadLine();
+                Console.WriteLine("hello" + i);
+            }
+
+        }
+    }
+}
