510d06822aeea79b6f559726b2d14f3719d02ccd
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-03-30 00:05:48 +0900

    Add files via upload

---
+++ for_while_scan/Seireki.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Seireki
+{
+    class Seireki
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var period = int.Parse(Console.ReadLine());
+            var syouwa = 0;
+
+            for (var i = start; i < period + start; i++)
+            {
+                Console.Write("西暦" + i + "年は");
+                syouwa = i - 1925;
+                Console.WriteLine("昭和" + syouwa + "年です");
+
+            }
+        }
+}
