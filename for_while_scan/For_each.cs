db4d62ec7883273d2db01433f9989bf3152a7cfa
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-03-30 23:44:19 +0900

    Add files via upload

---
+++ for_while_scan/For_each.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Foreach
+{
+    class For_each
+    {
+        static void Main(string[] args)
+        {
+            string[] team = { "勇者", "戦士", "魔法使い" };
+            //Console.WriteLine(team.Length);//データの個数   
+
+
+            foreach (string job in team)
+            {
+                //変数　in 取り出したい配列（変数）
+                Console.WriteLine(job);
+
+            }
+        }
+    }
+}
