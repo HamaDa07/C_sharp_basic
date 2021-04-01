bc2601467e65a5ab22ac2c0d56ebcb091a310e05
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-01 21:56:30 +0900

    Add files via upload

---
+++ loop_multi/Loop_Multi_basic2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Loop_Multi_basic2
+{
+    class Loop_Multi_basic2
+    {
+        static void Main(string[] args)
+        {
+            string[][] teams ={              // string[][] 変数名 {
+        new string[] {"A0","A1"}, //new string[] {"要素","要素"},
+        new string[] {"B0","B1","B2"}, //new string[] {"要素"}
+        new string[] {"C0","C1","C2"}, //};
+        new string[] {"D0"}
+        };
+            //new stringは、固定 //要素を加えるときに=(イコール)はいらない
+
+            Console.Write("[0][0]に入っているもの"+teams[0][0]);
+            //更新(再代入する)とき
+            teams[0][0] = "更新済み";
+
+            //配列の長さ 
+            Console.WriteLine("配列の個数（縦）"+teams.Length);//結果:4
+            Console.WriteLine("一つの配列の要素(横)" + +teams[0].Length);//結果2
+        }
+    }
+}
---
+++ loop_multi/Loop_multi_basic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loop_multi_basic
+{
+    class Loop_multi_basic
+    {
+        static void Main(string[] args)
+        {
+            string[] teamA = { "A0", "A1", "A2" };
+            string[] teamB = { "B0", "B1", "B2" };
+            string[] teamC = { "C0", "C1", "C2" };
+
+            string[][] teams = { teamA, teamB, teamC };
+            Console.Write(teams[0][0] + ",");
+            Console.Write(teams[0][1] + ",");
+            Console.Write(teams[0][1]);
+            //A1,A1,A2,
+            }
+        }
+    }
