ac90b4485339d69d8637bfbcb0a3b815ecd988af
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-05 21:24:08 +0900

    Add files via upload

---
+++ for_while_scan/Switch_basic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Switch_basic
+{
+    class Switch_basic
+    {
+        static void Main(string[] args)
+        {
+            var number = "";
+            Console.WriteLine("数字を入力してください");
+            number = Console.ReadLine();
+
+            switch(number)//switch(変数))
+            {
+                case "1": //case 値:
+                    Console.WriteLine("1です");
+                    break;
+
+                case "2":
+                    Console.WriteLine("2です");
+                    break;
+
+                default:
+                    Console.WriteLine("1,2以外の数字です");
+                    break;
+            
+            }
+
+
+
+           
+        }
+    }
+}
