91688187944139f9cefa2da7672b79fceff94773
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-05 21:51:44 +0900

    Add files via upload

---
+++ Switch_continue_goto/Switch_multicase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Switch_harder
+{
+    class Switch_multicase
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+            Console.WriteLine("1から5までの数字を入力してください");
+
+            number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                case 3:
+                case 5:
+                    Console.WriteLine((number)+"は奇数です");
+                    break;
+
+                case 2:
+                case 4:
+                    Console.WriteLine((number) + "は偶数です");
+                    break;
+
+                default:
+                    Console.WriteLine("エラーです");
+                    break;
+
+            }
+            
+
+        }
+    }
+}
