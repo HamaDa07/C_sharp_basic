23f857676c8adf8602f9c0cb827ae4ed43f138ea
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-05 22:36:11 +0900

    Add files via upload

---
+++ Switch_continue_goto/Goto_basic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Goto_basic
+{
+    class Goto_basic
+    {
+        static void Main(string[] args)
+        {
+            goto A;          // goto 任意のラベル;
+            Console.WriteLine("ここはスキップされる");
+
+            A://任意のラベル:
+            Console.WriteLine("コードが終了した");
+
+        }
+    }
+}
