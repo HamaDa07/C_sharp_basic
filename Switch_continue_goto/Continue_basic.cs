3b0ce20ff72a3185ef237489648f7d4ee418c41e
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-05 22:20:46 +0900

    Add files via upload

---
+++ Switch_continue_goto/Continue_basic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Continue_basic
+{
+    class Continue_basic
+    {
+        static void Main()
+        {
+            int ans = 0;
+
+            for(int i =0; i<100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    continue;//スキップする
+                }
+
+                ans += i;
+            }
+
+            Console.WriteLine("合計は"+(ans)+"です");
+        }
+    }
+}
