771abeb1a8f1fa3c1a26f862b17f34a1b9516573
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-06 21:03:23 +0900

    Add files via upload

---
+++ 配列（配列）/String_format.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace String_format
+{
+    class String_format
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("{0}は{1},{2}歳です", " おれ", "男", "3"));// おれは男,3歳です
+
+            Console.WriteLine(string.Format("{0}と{0}と{1}", "おれ", "あなた"));//おれとおれとあなた
+
+            Console.WriteLine(string.Format("日付:{0:D}", DateTime.Now));//日付:2021年4月6日
+
+            //{0}桁数分を0で置換、{D}は、10進数表記
+
+        }
+    }
+}
