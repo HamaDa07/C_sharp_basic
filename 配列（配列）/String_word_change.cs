97c168522ed7ed6d2a5968054bf088cfb72c3517
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-08 22:50:47 +0900

    Add files via upload

---
+++ 配列（配列）/String_word_change.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String
+{
+    class String_word_change
+    {
+        static void Main(string[] args)
+        {
+
+            var str1 = "A,B,C,D";
+            var str2 = str1.Replace("A", "0");
+
+            Console.WriteLine(str1);//A,B,C,D
+            Console.WriteLine(str2);//0,B,C,D
+        }
+    }
+}
