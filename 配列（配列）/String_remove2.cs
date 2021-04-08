d2510fa86ebdccd70a5c7e3e3771d495b4776ef1
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-08 22:40:21 +0900

    Add files via upload

---
+++ 配列（配列）/String_remove2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace String_remove2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var str1 = "ABC---DEF";
+            Console.WriteLine("最初の文字列" + str1);
+
+            Console.WriteLine("{0}", str1.Remove(3));
+            Console.WriteLine("{0}", str1.Remove(3, 3));
+        }
+    }
+}
