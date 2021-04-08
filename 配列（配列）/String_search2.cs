e27f15abbd7ac667afd6e96cbc20a89e16cd6af2
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-08 13:26:04 +0900

    Add files via upload

---
+++ 配列（配列）/String_search2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace String_search2
+{
+    class String_search2
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "ABCDEF";
+            Console.WriteLine(str1.Substring(3,2));//DE
+            // A B C D E F
+            //  1 2 3 4 5 
+
+            var str2 = "D://個人画像//horie.jpg";
+            Console.WriteLine(str2.Substring(str2.LastIndexOf(".") + 1));//jpg
+            Console.WriteLine(str2.Substring(str2.LastIndexOf(".")));//.jpg
+
+        }
+    }
+}
