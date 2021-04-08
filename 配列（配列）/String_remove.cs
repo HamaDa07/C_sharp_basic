99257f749c34d7e8c8c5a172f743ee60d3381ed3
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-08 22:20:57 +0900

    Update String_remove.cs

--- 配列（配列）/String_remove.cs
+++ 配列（配列）/String_remove.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace String_remove
 {
@@ -7,13 +7,21 @@ namespace String_remove
         static void Main(string[] args)
         {
 
-            var str1 = "ABCEDEFG";
+            var str1 = "ABCDEFG";
+            Console.WriteLine("元の文字列" + str1);
+            Console.WriteLine();
 
             var str2 = str1.Remove(0, str1.IndexOf("C"));//Cより前を消す
-            Console.WriteLine(str2);//CEDEFG
+            Console.WriteLine(str2);//CDEFG
+
+            var str0 = str1.Remove(1,str1.IndexOf("C"));
+            Console.WriteLine(str0);//ADEFG
+            var str3 = str1.Remove(2, str1.IndexOf("C"));
+            Console.WriteLine(str3);//ABEFG
+
 
             str2 = str2.Remove(str2.IndexOf('F'));//Fよりあとを消す
-            Console.WriteLine(str2);//CEDE
+            Console.WriteLine(str2);//CDE
         }
     }
 }
