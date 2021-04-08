43fb3a479420e448fac8431a12522d0c5c5280f5
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-08 21:52:19 +0900

    Add files via upload

---
+++ 配列（配列）/String_remove.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace String_remove
+{
+    class String_remove
+    {
+        static void Main(string[] args)
+        {
+
+            var str1 = "ABCEDEFG";
+
+            var str2 = str1.Remove(0, str1.IndexOf("C"));//Cより前を消す
+            Console.WriteLine(str2);//CEDEFG
+
+            str2 = str2.Remove(str2.IndexOf('F'));//Fよりあとを消す
+            Console.WriteLine(str2);//CEDE
+        }
+    }
+}
