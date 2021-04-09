95e2fd81dc98542aba09dabd6e6ae086c9bcfbc2
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-09 13:20:44 +0900

    Add files via upload

---
+++ 配列（配列）/String_format2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String_format2
+{
+    class String_format2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Format("名前は{0,5}です","ジョン"));
+            //名前は ジョンです 一番最後の文字が5になる
+
+
+            Console.WriteLine(string.Format("名前は{0,-5}です", "ジョン"));
+            //名前はジョン  です
+
+            Console.WriteLine(string.Format("名前は{0,-5}です", "K.ジョンソン"));
+            //名前はK.ジョンソンです
+            //入力したい文字のほうが文字数が多い場合は、空白は無効になる
+
+
+
+        }
+    }
+}
