1c23cd3627435c95ed81aa99849406ea56df6850
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-10 14:17:26 +0900

    Add files via upload

---
+++ 配列（配列）/String_formats/String_format_read.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace String_format_read
+{
+    class String_format_read
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("名前を入力してください");
+            var name = Console.ReadLine();
+            Console.WriteLine("年齢を入力してください");
+
+            var age = Console.ReadLine();
+
+            Console.WriteLine("{0}です。{1}歳です",name,age);
+        }
+    }
+}
