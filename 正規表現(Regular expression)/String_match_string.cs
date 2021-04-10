a35792edb2508e1571e0a94933567218dd2db52b
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-10 22:21:37 +0900

    Add files via upload

---
+++ 正規表現(Regular expression)/String_match_string.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace String_match_string
+{
+    class String_match_string
+    {
+        static void Main(string[] args)
+        {   
+            // "ABC"という文字列の繰り返しパターン
+            var words = "ABC";
+            var str1 = new[] { "ABC", "ABCABC", "ABCABCABC" };
+
+            var match1 = new Regex(@"(ABC){2,}");//"ABC"という文字列が2回以上のときに表示
+            foreach(var ans1 in str1)
+            {
+                Console.Write(ans1 + ":");
+                Console.WriteLine(match1.IsMatch(ans1) ? ans1 : "アンマッチ");
+            } //ABC:アンマッチ   ABCABC:ABCABC   ABCABCABC:ABCABCABC
+
+
+        }
+    }
+}
