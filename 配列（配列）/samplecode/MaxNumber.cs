1cb3c26166968593cd401927723c2748c92286c5
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-15 23:24:52 +0900

    Add files via upload

---
+++ 配列（配列）/samplecode/MaxNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            int [] array = new int [1100];
+            var max = 0;
+            for (var i=0; i < count;  i++)
+            {
+                array[i]=int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    max = array[i];
+                }
+
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+                   
+                
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
