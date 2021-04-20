f22f2521f992605cca348b48dbb9c7240cfb10f6
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-04-20 13:17:51 +0900

    Add files via upload

---
+++ loop_multi_and/Loop_multi_copy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Loop_multi_copy
+{
+    class Loop_multi_copy
+    {
+        static void Main(string[] args)
+        {
+            var multi = new string[,] { {"あ","い","う","え","お" }, {"A","B","C","D","E" } };
+            var multi2 = new string[2, 3];
+
+            //(3~6番目をコピー)
+            /////////////////////////////
+            /// 挙動確認////////////
+            /// ////////////////
+            Array.Copy(multi, 2, multi2, 0, 6);
+            foreach (var v in multi2)
+                Console.Write(v);
+            //うえおABC
+        }
+    }
+}
