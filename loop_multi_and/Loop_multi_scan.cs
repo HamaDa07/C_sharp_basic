f5b3bfc6ad6ce81e63c8fd168cf864da6201c6aa
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-04 14:51:41 +0900

    Add files via upload

---
+++ loop_multi_and/Loop_multi_scan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loop_multi_scan
+{
+    class Loop_multi_scan
+    {
+        static void Main(string[] args)
+        {   
+            //要素を代入
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int[][] numberB = new int[a][];
+
+            //2番めの[]の要素を代入する
+            for(int i=0; i<numberB.Length; i++)
+            {
+                numberB[i] = new int [b];
+            }
+
+            for(int i=0; i < numberB.Length; i++)//2
+            { 
+                for(int j=0; j<numberB[i].Length; j++)//5
+                {
+                    numberB[i][j] = i + j;
+                    Console.Write(numberB[i][j] + " ");
+                   /* 0 1 2 3 4
+                      1 2 3 4 5 */
+                }
+                Console.WriteLine();
+            }
+           
+
+        }
+    }
+}
