d5de2e86457ccce175e3454fcac8040fb9da816f
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-04 18:49:32 +0900

    Add files via upload

---
+++ Object指向/Method/Method_scope2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Method_scope2
+{
+    class Method_scope2
+    {
+        static void Main(string[] args)
+        {   
+            string message1 = "Hello ";
+            SayHello(message1);
+            //Hello Hello C_sharp
+        }
+        static string SayHello(string message1)
+        {   
+            
+            string message2 = "Hello";
+            message2 += message1;
+            message2 += " ";
+            message2 += "C_sharp";
+            Console.WriteLine(message2);
+            return message2;
+            //戻り値を返さないとエラーになるかも
+        }
+    }
+}
