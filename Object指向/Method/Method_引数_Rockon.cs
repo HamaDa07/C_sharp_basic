d20101b4c21dfd43d713b595824bd7a53a969e4c
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-05 12:03:05 +0900

    Add files via upload

---
+++ Object指向/Method/Method_引数_Rockon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_引数_Rockon
+{
+    class Method_引数_Rockon
+    {
+        static void Main(string[] args)
+        {
+            SayHello(greeting: "GoodMornig1 ");//GoodMornig1 ジョン
+            SayHello(name: "トム2 ");//Hello トム2
+            SayHello(name: "トムが先3", greeting:"やあ!" );//やあ!トムが先3
+        }
+        
+        static void SayHello(string greeting ="Hello ",string name = "ジョン")
+        {
+            Console.WriteLine(greeting + name);
+        }
+    }
+}
