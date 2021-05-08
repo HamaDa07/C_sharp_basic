1b516f82dec3a4a5c4b92d524cb430e71fecf13c
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-08 11:34:25 +0900

    Add files via upload

---
+++ Collections/Dicitionary/Dictionary _basic1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dicitornary_basic1
+{
+    class Dicitornary_basic1
+    {
+        static void Main(string[] args)
+        {   
+          //変数名　= new Dicionary <型名,型名>();
+            var dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(1, "A");
+            dictionary.Add(2, "B");
+            //     左側;Key,右側;Value    
+
+            var ravel1 = 2;
+            var ravel2 = "B";
+
+            Console.WriteLine(dictionary[1]);//A
+            Console.WriteLine(dictionary[ravel1]);//B
+
+            /*エラー↓
+            Console.WriteLine(dictionary[ravel2]);*/
+        }
+    }
+}
---
+++ Collections/Dicitionary/Dictionary _basic2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary_basic2
+{
+    class Dictionary_basic2
+    {
+        static void Main(string[] args)
+        {
+            var dicionary = new Dictionary<int,string>();
+
+            dicionary.Add(1, "One");
+            dicionary.Add(2,"Two");
+            dicionary.Add(3, "Three");
+            Console.WriteLine(dicionary[1]);
+            Console.WriteLine(dicionary[2]);
+            Console.WriteLine(dicionary[3]);
+
+            Console.WriteLine();
+            //要素数の確認    Count=3
+            Console.WriteLine("Count:"+dicionary.Count);
+
+            //Valueの置き換え
+            // 変数名[Key]= Value
+            dicionary[1] = "Change_One";
+            Console.WriteLine(dicionary[1]);
+
+            //要素のRemove
+            // 変数名.Remove(key)
+            dicionary.Remove(1);
+                
+
+
+
+
+        }
+    }
+}
