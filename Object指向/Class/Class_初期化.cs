fd447ce435082e3555288ad85ef2ec6d705a5d15
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-09 21:32:51 +0900

    Add files via upload

---
+++ Object指向/Class/Class_初期化.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Class_初期化
+{
+    class Class_初期化
+    {
+        static void Main(string[] args)
+        {
+            var show = new Person("コンストラクター"){ value = "オブジェクト初期化" };
+
+            Console.WriteLine(show.value);
+            //オブジェクト初期化
+
+            //優先順位
+            //1.オブジェクト初期化子{変数名=}
+            //2.コンストラクター   new クラス名 ()
+            //3.フィールド  public static ～～
+        }
+    }
+    class Person
+    {
+        public string value = "フィールド";
+
+        public Person() { }
+
+        public Person(string value)
+        {
+            this.value = value;
+        }
+    }
+
+}
+
+
+
