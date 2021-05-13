7ee937633aa4abc9b3d7b0fa6e990acc37cf9b20
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-05-13 12:26:12 +0900

    Add files via upload

---
+++ Object指向/Class/Class_インスタンス実行.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Class_インスタンス実行
+{
+    class Class_インスタンス実行
+    //インスタンス:クラスをnewしたもの
+    //このような普通なやり方だと、任意の戻り値が期待できない
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class2(); 
+            Console.WriteLine(c1.Print1());
+            //クラス1-print1
+
+            //↓エラー 子クラスから親クラスへの代入は不可
+           // Class2 c2 = new Class1();
+           // Console.WriteLine(c2.Print2());
+        }
+    }
+
+    class Class1
+    {
+        public string Print1()
+        {
+            return "クラス1-print1";
+        }
+    }
+
+    class Class2 :Class1
+    {
+        public string Print2()
+        {
+            return "クラス2-print2";
+        }
+    }
+}
