3ec3b3903cc276222983079a4278567886e49421
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-06-08 22:08:09 +0900

    Update File_text_write.cs

--- Collections/ファイル操作/File_text_write.cs
+++ Collections/ファイル操作/File_text_write.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 namespace File_text_write
 {
@@ -6,20 +6,31 @@ namespace File_text_write
     {
         static void Main(string[] args)
         {
-            using (var writer = new StreamWriter(@"D:\雑多\ちょっとしたもの\プログラミング関連\date.log")) 
+            using (var writer = new StreamWriter(@"D:\雑多\ちょっとしたもの\プログラミング関連\date.log", append: true))
             {
+                //append : true:追記する　false(上書きする)
                 writer.WriteLine(DateTime.Now.ToString());
-                //2021/06/08 19:58:23
-                //成功して、メモ帳からファイルを開くと↑のような記述
                 writer.WriteLine("成功!!!");
-                //成功!!!
                 int a = 10;
                 int b = 5;
                 string c = "数字文字列も可能";
 
-                writer.WriteLine("{0}+{1}={2}  {3}",a,b,a+b,c);
-                //10+5=15  数字文字列も可能
+                writer.WriteLine("{0}+{1}={2}  {3}", a, b, a + b, c);
+                //2021/06/08 19:58:23
+                //成功!!!
+                //10+5=15 数字文字列も可能
             }
+
+            //apend : false のとき     (上書き)
+            //2021/06/08 21:59:28
+            //成功!!!
+            //10 + 5 = 15  数字文字列も可能
+
+            //apemd : ture のとき      (追記)
+            /*2021/06/08 22:05:59
+            成功!!!
+            10+5=15  数字文字列も可能
+            */
         }
     }
 }
