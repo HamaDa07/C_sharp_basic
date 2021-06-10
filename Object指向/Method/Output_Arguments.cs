0c36d07c113f24a8ece5849d59d21f80d93260be
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-06-10 23:37:32 +0900

    Add files via upload

---
+++ Object指向/Method/Output_Arguments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 出力引数
+{
+    class Output_Arguments
+    {
+        public void GetMaxMin(int x,int y, out int max, out int min)
+        {
+            if (x >= y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var method = new Output_Arguments();
+            int resultMax, resultMin;
+
+            method.GetMaxMin(5, 3, out resultMax, out resultMin);
+            Console.WriteLine("resultMax="+ resultMax);//resultMax=5
+            Console.WriteLine("resultMin="+ resultMin);//resultMin=3
+
+        }
+    }
+}
