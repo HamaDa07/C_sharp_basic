3aa17a0b5ae7463477f83ecb085ac150c15f1fe6
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-06-13 23:02:13 +0900

    Add files via upload

---
+++ Object指向/Method/Multi_return.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Multi_return
+{
+    class Multi_return
+    {
+
+        public IEnumerable<string> Getstring()
+           //Ienumerable <型名>　メッソド名
+        {
+            yield return "ABC";
+            yield return "DEF";
+            yield return "GHI";
+        }
+
+
+        static void Main(string[] args)
+        {
+            var ite = new Multi_return();
+
+            foreach (var str in ite.Getstring())
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
