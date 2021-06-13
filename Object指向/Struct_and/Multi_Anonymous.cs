6f9e557809859d6733f649c169b47f7dfc9d4560
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-06-13 22:45:15 +0900

    Add files via upload

---
+++ Object指向/Struct_and/Multi_Anonymous.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Multi_Anonymous
+{
+    class Multi_Anonymous
+    {
+        static void Main(string[] args)
+        {
+            var info = new { name = "ジョン", age = 22 ,weight=45 };
+            Console.WriteLine(info.name);
+            Console.WriteLine(info.age);
+
+            //エラー　→　info.weight = 33;
+
+            var info2 = new { info.name, info.age, info.weight };
+
+
+            Console.WriteLine(info.weight);
+
+        }
+    }
+}
---
+++ Object指向/Struct_and/Struct_basic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Struct_basic
+{
+    class Struct_basic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    struct Player
+    {
+
+    }
+}
